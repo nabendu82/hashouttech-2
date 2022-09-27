3fcfffc216fc6cd84af77e6f69bb0da7d98e166c
nabendu82 <nabendu.biswas@gmail.com>
2022-09-27 09:58:54 +0530

    React -1

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import EventBind from './components/EventBind';
+// import Counter from './components/Counter';
+// import { Greet, ReactGreet } from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Greet />
+      <ReactGreet /> */}
+      {/* <Greeting name="JavaScript">Language of the Web</Greeting>
+      <Greeting name="Golang" />
+      <Greeting name="Swift">Language for iOS app</Greeting>
+      <hr />
+      <Welcome name="Flutter">Language for cross-platform mobile development</Welcome>
+      <Welcome name="React Native" />
+      <Welcome name="Java"><button>Click for Java</button></Welcome> */}
+      {/* <Counter /> */}
+      <EventBind />
+    </div>
+  );
+}
+
+export default App;
