00a0e84be3587c30e956aed3d91c28f6db13f218
nabendu82 <nabendu.biswas@gmail.com>
2022-10-20 09:55:03 +0530

    Hooks -1

---
+++ hook/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounter />
+    </div>
+  );
+}
+
+export default App;
