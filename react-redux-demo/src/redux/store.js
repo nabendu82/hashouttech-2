d911eb2804e1c5463d354c931caab9148b7782e8
nabendu82 <nabendu.biswas@gmail.com>
2022-11-02 10:25:01 +0530

    React redux done

---
+++ react-redux-demo/src/redux/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./rootReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+
+export default store
